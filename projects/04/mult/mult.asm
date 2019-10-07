8127629f92233ae93a048be676856fa59d637712
vineethMM <vineeth990@gmail.com>
2019-10-07 20:03:02 +1100

    Implement multiplication

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,51 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// initialise accumulator
+   @acc // declare a variable res which will act as accumulator
+   M=0  // initialise accumulator to 0
+
+// declare the loop variable
+   @R1
+   D=M // set D register to value at R1
+
+   @i
+   M=D // initialise the loop variable to value at R1
+
+   @LOOP
+   0;JMP // proceed to LOOP
+
+(LOOP)
+   @i
+   D=M // fetch loop variable
+
+   @STOP
+   D; JEQ // if i = 0 then STOP
+
+   @acc
+   D=M // fetch acc value
+
+   @R0
+   D=D+M // acc = acc + R0
+
+   @acc
+   M=D  // store it back in acc
+
+   @i
+   M=M-1 // i--
+
+   @LOOP
+   0;JMP
+
+(STOP)
+   @acc
+   D=M
+   @R2
+   M=D // Store accumulator to R2 register
+   @END
+   0;JMP
+
+// program termination
+(END)
+   @END
+   0;JMP
